c4d3db9b761155d9ed0cbef96830499f9e78c660
Sahil Jajodia <sahil.jajodia@gmail.com>
2019-06-30 13:15:29 +0530

    Add kubeconfig path option

--- switch_cluster.py
+++ switch_cluster.py
@@ -1,2 +1,8 @@
-def cli():
-    print("Hello World")
+import click
+import os
+from kubernetes import client, config
+
+@click.command()
+@click.option('--kubeconfig', type=click.Path(), default=os.environ['HOME'] + '/.kube/config', help='Path of the administrator Kubeconfig file')
+def cli(**kwargs):
+    click.echo("Kubeconfig path is: {}".format(kwargs['kubeconfig']))
